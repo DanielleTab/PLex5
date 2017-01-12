926dcdb992f55d67df64206d71cf05246545ebe8
Daniel Feldman <cool@gmail.com>
2017-01-12 21:07:19 +0200

    added comments

--- pl17_ex5/minesw/mineswex.js
+++ pl17_ex5/minesw/mineswex.js
@@ -58,11 +58,17 @@ function nxt() {
         $(this).text(value);
 
         /* ADD YOUR CODE HERE */
+        
+        /* if the value is non zero - stop */
         if (0 != value)
         {
             return;
         }
-        
+
+        /* this function limit the indexes to the board's size =>
+                row: from 0 to SZ 
+                col: from 0 to SZ 
+        */
         var handleBoardSizeColRowIndex = function(num)
                                 {
                                     if (0 > num)
@@ -77,18 +83,25 @@ function nxt() {
                                     return num;
                                 }
 
+        /****
+        * This block iterate over the cells around the current (zero) reveal cell (only 1 hop).
+        *  On each cell which is hidden we call:  cell.fadeIn(nxt);
+        *       => this cause the cell to be shown and to the recursive reveal of zeros to keep going.
+        ******/
         var indexRow = -1;
         for( ; indexRow < 2; indexRow = indexRow + 1)
         {
             var indexCol = -1;
             for( ; indexCol < 2; indexCol = indexCol + 1)
             {
+                /* get correct indexes (we dont want indexes >= SZ or indexes < 0) */
                 var realRowIndex = handleBoardSizeColRowIndex(row + indexRow);
                 var realColIndex = handleBoardSizeColRowIndex(col + indexCol);
                 
                 var curCell = get_cell(realRowIndex, realColIndex);
                 if (true === is_cell_hidden(curCell))
                 {
+                    /* reveal the cell and keep revealing zeros around the cell */
                     curCell.fadeIn(nxt);
                 }
             }            
